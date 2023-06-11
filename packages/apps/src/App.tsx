6e57a6077955b5c4da39e946d54a63fb36d1370c
Dabee-ee <movie2529@naver.com>
2023-06-11 23:47:15 +0900

    chore: init project

---
+++ packages/apps/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from '@mono/common'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button>모노레포 테스트</Button>
+      
+    </>
+  )
+}
+
+export default App
