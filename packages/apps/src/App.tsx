ed3c1e2dee1ae031be89ed84cc9f68660395f580
Dabee-ee <movie2529@naver.com>
2023-06-12 00:17:35 +0900

    feat: add `common-ui` packages

--- packages/apps/src/App.tsx
+++ packages/apps/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import { Button } from '@mono/common'
+import { Button } from '@mono/common';
+import {Input} from "@mono/ui";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Button>모노레포 테스트</Button>
+      <Input />
       
     </>
   )
